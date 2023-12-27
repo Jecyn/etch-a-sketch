5cbd5981f28dd8f62e4bf6aa4478871cd849569c
Jecyn <jessewatson51495@gmail.com>
2023-12-27 14:29:58 -0600

    added hovering color changing effect

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let gridSize = 16
 const page = document.querySelector('#grid-container');
 
 function createGrid(size){
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
-            let div = createDiv()
-            div.textContent = i;
+            let div = createDiv(size)
             page.appendChild(div)
-            
         }
-        
     }
 }
 
-function createDiv(){
+function createDiv(size){
     let square = document.createElement('div');
     square.classList.add('grid-square')
+    square.addEventListener("mouseover", changeColor)
     return square
 }
 
+function changeColor(e){
+    e.target.style.backgroundColor = 'darkgreen'
+}
+
 createGrid(16)
+
+
